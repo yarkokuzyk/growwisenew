da3cb3271e2e3ea42d59bffb835a1697c8fc26db
yarkokuzyk <slavikkuzik1@gmail.com>
2023-05-18 00:03:52 +0300

    Create Profile Controller, AppUserServices

---
+++ Growwise.Data/IApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using Growwise.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Growwise.Data
+{
+    public interface IApplicationUser
+    {
+        ApplicationUser GetById(string id);
+        IEnumerable<ApplicationUser> GetAll();
+
+        Task SetProfileImageAsync(string id, Uri uri);
+        Task IncrementRating(string id, Type type);
+
+    }
+}
---
+++ Growwise1/Controllers/ProfileController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Growwise.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Growwise1.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IApplicationUser _userService;
+        private readonly IUpload _uploadService;
+
+        public ProfileController(UserManager<ApplicationUser> userManager,
+            IApplicationUser userService,
+            IUpload uploadService)
+        {
+            _userManager = userManager;
+            _userService = userService;
+            _uploadServce = _uploadService;
+        }
+
+        public IActionResult Detail(string id)
+        {
+            return View();
+        }
+
+    }
+}
