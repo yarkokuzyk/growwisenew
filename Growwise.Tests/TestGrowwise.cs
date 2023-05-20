7fc994d8b17bc284343fe38445edd98a9325b19d
yarkokuzyk <slavikkuzik1@gmail.com>
2023-05-20 23:12:41 +0300

    pre azure

---
+++ Growwise.Tests/TestGrowwise.cs
@@ -0,0 +1,67 @@
+﻿using Growwise.Data;
+using Growwise.Data.Models;
+using Growwise.Service;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace Growwise.Tests
+{
+    [TestFixture]
+    public class SearchServiceTests
+    {
+        [TestCase("coffee", 3)]
+        [TestCase("teA", 1)]
+        [TestCase("waTer", 0)]
+        public void ReturnFilteredResultsCorrespondingToQuery(string query, int expected)
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            using (var ctx = new ApplicationDbContext(options))
+            {
+                ctx.Forums.Add(new Forum
+                {
+                    Id = 19
+                });
+
+                ctx.Posts.Add(new Post
+                {
+                    Forum = ctx.Forums.Find(19),
+                    Id = 23556,
+                    Title = "First Post",
+                    Content = "Coffee"
+                });
+                ctx.Posts.Add(new Post
+                {
+                    Forum = ctx.Forums.Find(19),
+                    Id = -236,
+                    Title = "Coffee",
+                    Content = "Some Content"
+                });
+                ctx.Posts.Add(new Post
+                {
+                    Forum = ctx.Forums.Find(19),
+                    Id = 2356,
+                    Title = "Tea",
+                    Content = "Coffee"
+                });
+
+                ctx.SaveChanges();
+            }
+
+            using (var ctx = new ApplicationDbContext(options))
+            {
+                var postService = new PostService(ctx);
+
+                var result = postService.GetFilteredPosts(query);
+
+                var postCount = result.Count();
+
+                Assert.AreEqual(expected, postCount);
+            }
+        }
+    }
+}
